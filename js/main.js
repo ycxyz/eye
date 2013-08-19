ee2bcf469d8226406a4d2db326c6850159529b8e
UncleBill <billbill290@gmail.com>
2013-08-19 14:17:20 +0800

    update from yang

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ $(function(){
 	// },function(){
 	// 	$(this).css('opacity','0.3');
 	// })
+	// 
+	$('.dropdown').hover(function(e){
+		$(this).find('.mega-wrap').fadeIn();
+		$(this).find('a').eq(0).addClass('bg');
+	},function(){
+		$(this).find('.mega-wrap').fadeOut('slow');
+		$(this).find('a').eq(0).removeClass('bg');
+	});
 });
